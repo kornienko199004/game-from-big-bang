cfcd28af8525fecc38972bcd3397dd0814b17c75
kornienko199004 <kornienko199004@gmail.com>
2020-01-02 12:10:28 +0300

    Add joinlink

--- public/js/game.js
+++ public/js/game.js
@@ -1,1 +1,25 @@
 const socket = io();
+const link = document.querySelector('#link');
+const linkTemplate = document.querySelector('#link-template').innerHTML;
+
+const { room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
+
+socket.emit('join', { room }, (error) => {
+  if (error) {
+    alert(error);
+    location.href = '/';
+  }
+});
+
+socket.on('return room', (lastRoom) => {
+  const joinLink = new URL(location);
+  joinLink.searchParams.append('room', lastRoom);
+
+  // should show link
+
+  const html = Mustache.render(linkTemplate, {
+    link: joinLink.toString(),
+});
+link.insertAdjacentHTML('beforeend', html);
+
+});
--- src/index.js
+++ src/index.js
@@ -10,10 +10,41 @@ const io = socketio(server);
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+let lastRoom = 0;
+
 app.use(express.static(publicDirectoryPath));
 
 io.on('connection', socket => {
     console.log('New websocket connection');
+
+    socket.on('join', (options, callback) => {
+      console.log(options);
+
+      if (options.room) {
+        // start the game
+      } else {
+        // return url
+        socket.emit('return room', lastRoom);
+        lastRoom += 1;
+      }
+      // const { error, user } = addUser({ id: socket.id, ...options });
+
+      // if (error) {
+      //     return callback(error);
+      // }
+      // socket.join(user.room);
+      // socket.emit('message', generateMessage('Admin', 'Welcome'));
+      // socket.broadcast
+      //     .to(user.room)
+      //     .emit('message', generateMessage('Admin', `${user.username} has joined!`));
+
+      // io.to(user.room).emit('roomData',{
+      //     room: user.room,
+      //     users: getUsersInRoom(user.room),
+      // });
+
+      callback();
+  });
 });
 
 server.listen(port, () => {
